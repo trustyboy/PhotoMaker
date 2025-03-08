8ceffae6e87008496a0ca6d7fc892f649848e543
guofeng <alex@fzwise.com>
2025-03-08 21:08:55 +0800

    阶段性代码

--- gradio_demo/app_v2.py
+++ gradio_demo/app_v2.py
@@ -59,7 +59,7 @@ pipe = PhotoMakerStableDiffusionXLAdapterPipeline.from_pretrained(
     variant="fp16",
 ).to(device)
 
-#pipe.enable_vae_slicing()
+pipe.enable_vae_slicing()
 
 pipe.load_photomaker_adapter(
     os.path.dirname(photomaker_ckpt),
